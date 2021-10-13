7fb7dc3b7092a43ba48a1bed86407ebc68400511
adams892290 <adams89220-@gmail.com>
2021-10-13 20:33:50 +0530

    id

---
+++ dist/plugins/tubular/js/index.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+	var options = { videoId: 'G52dUQLxPzg', start:30 };
+	$('#wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
